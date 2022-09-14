a783c46a20b6e48c690267735a10560ebf233cd2
SamsonVacity <qq786267360>
2022-09-14 15:42:57 +0800

    删除homeController的@controller注解

--- src/main/java/com/tacocloud/controller/HomeController.java
+++ src/main/java/com/tacocloud/controller/HomeController.java
@@ -3,7 +3,7 @@ package com.tacocloud.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+
 public class HomeController {
 
     @GetMapping("/")
